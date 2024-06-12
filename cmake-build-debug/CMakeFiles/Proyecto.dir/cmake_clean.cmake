fe69e6256acad41c5ce48991d4c675d4f3bad767
Marti <martina.sandoval.o@mail.pucv.cl>
2024-06-11 22:57:21 -0400

    copia proyecto

---
+++ cmake-build-debug/CMakeFiles/Proyecto.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proyecto.dir/Game.c.obj"
+  "CMakeFiles/Proyecto.dir/Game.c.obj.d"
+  "CMakeFiles/Proyecto.dir/TDAs/List.c.obj"
+  "CMakeFiles/Proyecto.dir/TDAs/List.c.obj.d"
+  "Proyecto.exe"
+  "Proyecto.exe.manifest"
+  "Proyecto.pdb"
+  "libProyecto.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Proyecto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

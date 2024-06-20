bb0a7d82cffdecb2100cc9b70505906e802bd202
Isidora Osorio <isidora.osorio.n@mail.pucv.cl>
2024-06-20 18:16:01 -0400

    cambios de Cmake

---
+++ build/CMakeFiles/Proyecto.dir/TDAs/Map.c.obj.d
@@ -0,0 +1,23 @@
+CMakeFiles/Proyecto.dir/TDAs/Map.c.obj: \
+ C:/Users/isica/OneDrive/Documentos/Codigo/Estructura/GameProject/TDAs/Map.c \
+ C:/Users/isica/OneDrive/Documentos/Codigo/Estructura/GameProject/TDAs/map.h \
+ C:/Users/isica/OneDrive/Documentos/Codigo/Estructura/GameProject/TDAs/list.h \
+ C:/msys64/mingw64/include/stdio.h \
+ C:/msys64/mingw64/include/corecrt_stdio_config.h \
+ C:/msys64/mingw64/include/corecrt.h C:/msys64/mingw64/include/_mingw.h \
+ C:/msys64/mingw64/include/_mingw_mac.h \
+ C:/msys64/mingw64/include/_mingw_secapi.h \
+ C:/msys64/mingw64/include/vadefs.h \
+ C:/msys64/mingw64/include/sdks/_mingw_ddk.h \
+ C:/msys64/mingw64/include/_mingw_off_t.h \
+ C:/msys64/mingw64/include/swprintf.inl \
+ C:/msys64/mingw64/include/sec_api/stdio_s.h \
+ C:/msys64/mingw64/include/stdlib.h \
+ C:/msys64/mingw64/include/corecrt_wstdlib.h \
+ C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/13.2.0/include/limits.h \
+ C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/13.2.0/include/syslimits.h \
+ C:/msys64/mingw64/include/limits.h C:/msys64/mingw64/include/crtdefs.h \
+ C:/msys64/mingw64/include/sec_api/stdlib_s.h \
+ C:/msys64/mingw64/include/malloc.h \
+ C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/13.2.0/include/mm_malloc.h \
+ C:/msys64/mingw64/include/errno.h
